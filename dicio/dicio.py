1abfebb4dfc7830f6fc69928336c7555c56091a2
felipemfp <felipemfpontes@gmail.com>
2016-01-18 23:03:07 -0300

    Implementa novas funções
    Função Sinônimos
    Função Adicionais

--- dicio/dicio.py
+++ dicio/dicio.py
@@ -4,26 +4,40 @@ Python API não oficial para Dicio.com.br
 @email felipemfpontes@gmail.com
 """
 
+import html
 from urllib import request
 from utils import *
 
 BASE_URL = "http://www.dicio.com.br/{}"
 CHARSET = "iso-8859-1"
 TAG_MEANING = ("id=\"significado\"", "</p>")
+TAG_SYNONYMS = ("class=\"adicional sinonimos\"", "</p>")
+TAG_SYNONYMS_DELIMITER = ("<a", "</a>")
 TAG_ENCHANT = ("id=\"enchant\"", "</div>")
+TAG_EXTRA = ("class=\"adicional\"", "</p>")
+TAG_EXTRA_SEP = "br"
+TAG_EXTRA_DELIMITER = ("<b>", "</b>")
 
 class Word(object):
-    def __init__(self, word, meaning = None):
+    def __init__(self, word, meaning = None, synonyms = [], extra = {}):
         self.word = word
-        self.url = BASE_URL.format(word)
+        self.url = BASE_URL.format(remove_accents(word).strip().lower())
         self.meaning = meaning
+        self.synonyms = synonyms
+        self.extra = extra
 
     def __repr__(self):
         return self.word
 
+    def load(self):
+        found = Dicio().search(self.word)
+        self.meaning = found.meaning
+        self.synonyms = found.synonyms
+        self.extra = found.extra
+
 class Dicio(object):
     """
-    Dicio API com significado.
+    Dicio API com significado, sinônimos e adicionais.
     """
     def search(self, word):
         """
@@ -32,15 +46,17 @@ class Dicio(object):
         if len(word.split()) > 1:
             return None
 
-        word = remove_accents(word).strip().lower()
-        url = request.urlopen(BASE_URL.format(word))
-        page = url.read().decode(CHARSET)
+        _word = remove_accents(word).strip().lower()
+        url = request.urlopen(BASE_URL.format(_word))
+        page = html.unescape(url.read().decode(CHARSET))
 
         if page.find(TAG_ENCHANT[0]) > -1:
             return None
 
         found = Word(word)
         found.meaning = self.meaning(page)
+        found.synonyms = self.synonyms(page)
+        found.extra = self.extra(page)
 
         return found
 
@@ -51,10 +67,46 @@ class Dicio(object):
         """
         return remove_spaces(remove_tags(text_between(page, TAG_MEANING[0], TAG_MEANING[1], True)))
 
+    def synonyms(self, page):
+        """
+        Retorna os sinônimos encontrados.
+        """
+        synonyms = []
+        if page.find(TAG_SYNONYMS[0]) > -1:
+            synonyms_html = text_between(page, TAG_SYNONYMS[0], TAG_SYNONYMS[1], True)
+            while synonyms_html.find(TAG_SYNONYMS_DELIMITER[0]) > -1:
+                synonym = text_between(synonyms_html, TAG_SYNONYMS_DELIMITER[0], TAG_SYNONYMS_DELIMITER[1], True)
+                synonyms.append(Word(remove_spaces(synonym)))
+                synonyms_html = synonyms_html.replace(TAG_SYNONYMS_DELIMITER[0], "", 1)
+                synonyms_html = synonyms_html.replace(TAG_SYNONYMS_DELIMITER[1], "", 1)
+        return synonyms
+
+
+    def extra(self, page):
+        """
+        Retorna os adicionais encontrados.
+        """
+        dic_extra = {}
+        if page.find(TAG_EXTRA[0]) > -1:
+            extra_html = text_between(page, TAG_EXTRA[0], TAG_EXTRA[1], True)
+            extra_rows = split_html_tag(remove_spaces(extra_html), TAG_EXTRA_SEP)
+            for row in extra_rows:
+                _row = remove_tags(row)
+                key, value = _row.split(":")
+                dic_extra[remove_spaces(key)] = remove_spaces(value)
+        return dic_extra
+
 
 if __name__ == "__main__":
-    word = Dicio().search("curupira")
+    word = Dicio().search("Doce")
     if word:
         print(word, word.url, "\n\t", word.meaning)
+        print("Sinônimos")
+        for item in word.synonyms:
+            item.load()
+            print("\t", item, item.url, item.meaning)
+        print("Extras")
+        for key, value in word.extra.items():
+            print("\t", key, "=>", value)
     else:
         print("Não encontrado.")
--- dicio/utils.py
+++ dicio/utils.py
@@ -8,6 +8,31 @@ def remove_tags(str):
     import re
     return re.sub('<[^>]*>', ' ', str).strip()
 
+def text_before(str, after):
+    """
+    Retorna o texto antes da string after
+
+    >>> text_before("<a href='#'>Something</a>", "</a>")
+    "<a href='#'>Something"
+    """
+    index = str.find(after)
+    if index > -1:
+        return str[:index]
+    return str
+
+def text_after(str, before):
+    """
+    Retorna o texto depois da string before
+
+    >>> text_after("<a href='#'>Something</a>", "<a href='#'>")
+    'Something</a>'
+    """
+    index = str.find(before)
+    if index > -1:
+        index += len(before)
+        return str[index:]
+    return str
+
 def text_between(str, before, after, forceHTML = False):
     """
     Retorna o texto entre as string before e after
@@ -15,7 +40,9 @@ def text_between(str, before, after, forceHTML = False):
     >>> text_between("<a href='#'>Something</a>", "<a href='#'>", "</a>")
     'Something'
     """
-    start = str.find(before) + len(before)
+    start = str.find(before)
+    if start > -1:
+        start += len(before)
     if forceHTML:
         if before[-1] != ">":
             start = str.find(">", start) + 1
@@ -25,12 +52,15 @@ def text_between(str, before, after, forceHTML = False):
             end = str.find("<", start)
     if -1 < start < end:
         return str[start:end]
-    return str.strip()
+    return str
 
 def remove_spaces(str):
     """
     Retorna uma nova string com todos os espaços duplos, tabulações
     e quebras de linha removidos
+
+    >>> remove_spaces("Something  else")
+    'Something else'
     """
     str = str.replace("\t", " ")
     str = str.replace("\n", " ")
@@ -50,13 +80,15 @@ def remove_accents(str):
               "é", "è", "ê", "ë",
               "í", "ì", "î", "ï",
               "ó", "ò", "ô", "õ", "ö",
-              "ú", "ù", "û", "ü"]
+              "ú", "ù", "û", "ü",
+              "ç"]
 
     decode = ["a", "a", "a", "a", "a",
               "e", "e", "e", "e",
               "i", "i", "i", "i",
               "o", "o", "o", "o", "o",
-              "u", "u", "u", "u"]
+              "u", "u", "u", "u",
+              "c"]
 
     out = ""
     found = False
@@ -73,6 +105,28 @@ def remove_accents(str):
             out += chr
 
     return out
+
+def split_html_tag(str, tag):
+    """
+    Retorna split para determinada tag html dentro dos formatos possíveis
+
+    >>> str = "Something<br>else<br />and<br/>another<br></br>thing"
+    >>> split_html_tag(str, "br")
+    ['Something', 'else', 'and', 'another', 'thing']
+    """
+    TEMPLATE = "<{0} />"
+    templates = ["<{0}></{0}>",
+               "<{0}></ {0}>",
+               "<{0} ></{0}>",
+               "<{0} ></ {0}>",
+               "<{0}>",
+               "<{0}/>",
+               "<{0} >"]
+
+    new_str = str
+    for template in templates:
+        new_str = new_str.replace(template.format(tag), TEMPLATE.format(tag))
+    return list(filter(None, new_str.split(TEMPLATE.format(tag))))
 
 if __name__ == "__main__":
     import doctest
